91350c4c1ae6f146c23124c8816e5c38b4752bc7
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-02-14 21:17:37 +0500

    .

--- my_scripts/src/goto_goal.py
+++ my_scripts/src/goto_goal.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python2
 
 import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+
+class node:
+    def __init__(self):
+        rospy.init_node('goto_goal', anonymous=True)
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.sub = rospy.Subscriber('/odom', Odometry, self.callback)
+        
+        self.pose = Odometry()
+        self.orientation_list = []
+        self.msg = Twist()
+        self.rate = rospy.Rate(10)
+        
+        def callback(self, msg):
+            self.pose = msg
+            self.orientation_list = [self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z, self.pose.orientation.w](roll, pitch, yaw) = euler_from_quaternion(self.orientation_list)
+            
