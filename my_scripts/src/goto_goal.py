ceaad711c06b956a78c84a077218fbe58d647dc3
Abdullah-E <abdullahehsan4242@gmail.com>
2024-02-14 22:10:26 +0500

    biig

--- my_scripts/src/goto_goal.py
+++ my_scripts/src/goto_goal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point, Quaternion, Pose
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
@@ -11,17 +11,34 @@ class node:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.sub = rospy.Subscriber('/odom', Odometry, self.callback)
         
-        self.pose = Odometry()
+        self.angles = {}
+        self.angle_names = ['roll', 'pitch', 'yaw']
+        
+        self.odom = Odometry()
+        self.position = Point()
+        self.orient = Quaternion()
         self.orientation_list = []
         self.msg = Twist()
         self.rate = rospy.Rate(10)
         
     def callback(self, msg):
-        self.pose = msg.pose
-        
+        self.odom = msg
+        self.position = self.odom.pose.pose.position
+        self.orient = self.odom.pose.pose.orientation
+        euler_angs = euler_from_quaternion([self.orient.x, self.orient.y, self.orient.z, self.orient.w])
+        for i, n in enumerate(self.angle_names):
+            self.angles[n] = euler_angs[i]
+    
+    def move2goal(self):
+        goal_pose = Pose()
+        goal_pose.x = 
     def print_pose(self):
-        print("x: ", self.pose.pose.position.x)
-        print(self.pose)
+        # print("x: ", self.pose.pose.pose.position.x)
+        # print(self.pose)
+        # print(self.pose.pose.pose.position)
+        print("postion:", self.position)
+        print("orientation:", self.orient)
+        print("angles:", self.angles)
         self.rate.sleep()
             
 if __name__ == "__main__":
